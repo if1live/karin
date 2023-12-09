0b1887de297c9bcb3201ad29d5de78eea102ca69
if1live <libsora25@gmail.com>
2023-12-10 02:38:52 +0900

    created_at 예제

--- test/instances/rdbms.test.ts
+++ test/instances/rdbms.test.ts
@@ -1,85 +1,177 @@
 import { strict as assert } from "node:assert";
-import { after, before, describe, it } from "node:test";
-import { Generated, Selectable, SqliteAdapter, sql } from "kysely";
+import { after, before, describe, it, skip } from "node:test";
+import { Generated, Kysely, RawBuilder, SqliteAdapter, sql } from "kysely";
 import { createKysely, selectDialect } from "../../src/instances/rdbms.js";
-import { Json } from "../../src/tables.js";
+import { Timestamp } from "../../src/tables.js";
+
+/**
+ * json example
+ * @link https://github.com/kysely-org/kysely/blob/0.26.1/test/node/src/json.test.ts
+ */
+interface JsonTable {
+  id: Generated<number>;
+  data_notnull: object;
+  data_nullable: object | null;
+}
+const JsonTable = {
+  async prepare(db: Kysely<Database>) {
+    await db.schema
+      .createTable("json")
+      .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
+      .addColumn("data_notnull", "json", (col) => col.notNull())
+      .addColumn("data_nullable", "json")
+      .execute();
+  },
+};
 
 interface User {
   id: Generated<number>;
   name: string;
-  json: Json;
 }
+const User = {
+  async prepare(db: Kysely<Database>) {
+    await db.schema
+      .createTable("user")
+      .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
+      .addColumn("name", "varchar(255)", (col) => col.notNull().unique())
+      .execute();
+  },
+};
+
+interface Article {
+  id: Generated<number>;
+  title: string;
+  created_at: Generated<Timestamp>;
+  updated_at: Generated<Timestamp>;
+}
+const Article = {
+  async prepare(db: Kysely<Database>) {
+    await db.schema
+      .createTable("article")
+      .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
+      .addColumn("title", "varchar(255)", (col) => col.notNull().unique())
+      .addColumn("created_at", "datetime(6)", (col) =>
+        col.defaultTo(sql`current_timestamp`).notNull(),
+      )
+      .addColumn("updated_at", "datetime(6)", (col) =>
+        // TODO: sqlite에서는 updated_at을 trigger로 구현해야 한다.
+        // 이렇게까지 하고싶진 않아서 전체 스펙을 구현하진 않는다.
+        col
+          .defaultTo(sql`current_timestamp`)
+          .notNull(),
+      )
+      .execute();
+  },
+};
 
 interface Database {
   user: User;
+  article: Article;
+  json: JsonTable;
 }
 
 describe("rdbms", async () => {
   const dialect = await selectDialect()();
   const db = createKysely<Database>(dialect);
 
-  before(async () => {
-    await db.schema
-      .createTable("user")
-      .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
-      .addColumn("name", "varchar(255)", (col) => col.notNull().unique())
-      .addColumn("json", "json")
-      .execute();
-  });
+  before(async () => {});
 
   after(async () => {
     await db.destroy();
   });
 
-  it("sqlite", () => {
-    const adapter = db.getExecutor().adapter;
-    assert.ok(adapter instanceof SqliteAdapter === true);
+  describe("core", () => {
+    it("sqlite", () => {
+      const adapter = db.getExecutor().adapter;
+      assert.ok(adapter instanceof SqliteAdapter === true);
+    });
   });
 
-  it("insert", async () => {
-    // JSON.stringify() 거치지 않으면 에러 발생
-    // TypeError [Error]: SQLite3 can only bind numbers, strings, bigints, buffers, and null
-    await db
-      .insertInto("user")
-      .values({ name: "foo", json: JSON.stringify(["a"]) })
-      .execute();
+  describe("simple", () => {
+    before(async () => User.prepare(db));
+    after(async () => db.schema.dropTable("user").execute());
 
-    // null은 그냥 넣어도 잘 들어간다.
-    await db.insertInto("user").values({ name: "bar", json: null }).execute();
+    it("insert", async () => {
+      await db
+        .insertInto("user")
+        .values([{ name: "foo" }, { name: "bar" }])
+        .execute();
+    });
+
+    it("find all", async () => {
+      const founds = await db.selectFrom("user").selectAll().execute();
+      assert.equal(founds.length, 2);
+    });
+
+    it("find: foo", async () => {
+      const found_foo = await db
+        .selectFrom("user")
+        .where("name", "=", "foo")
+        .selectAll()
+        .executeTakeFirstOrThrow();
+
+      assert.equal(found_foo.name, "foo");
+    });
   });
 
-  it("find all", async () => {
-    const founds = await db.selectFrom("user").selectAll().execute();
-    assert.equal(founds.length, 2);
+  describe("json", () => {
+    before(async () => JsonTable.prepare(db));
+    after(async () => db.schema.dropTable("json").execute());
+
+    it("insert", async () => {
+      const toJson = <T>(obj: T): RawBuilder<T> => sql`${JSON.stringify(obj)}`;
+
+      // JSON.stringify() 거치지 않으면 에러 발생
+      // TypeError [Error]: SQLite3 can only bind numbers, strings, bigints, buffers, and null
+      await db
+        .insertInto("json")
+        .values({
+          data_notnull: toJson(["a"]),
+          data_nullable: null,
+        })
+        .execute();
+    });
+
+    it("find all", async () => {
+      const found = await db
+        .selectFrom("json")
+        .selectAll()
+        .executeTakeFirstOrThrow();
+      assert.deepEqual(found.data_notnull, ["a"]);
+      assert.equal(found.data_nullable, null);
+    });
   });
 
-  it("find: foo", async () => {
-    const found_foo = await db
-      .selectFrom("user")
-      .where("name", "=", "foo")
-      .selectAll()
-      .executeTakeFirstOrThrow();
+  describe("datetime", () => {
+    before(async () => {
+      await Article.prepare(db);
+    });
+    after(async () => db.schema.dropTable("article").execute());
 
-    assert.equal(found_foo.name, "foo");
-    assert.deepEqual(found_foo.json, ["a"]);
+    it("insert + select", async () => {
+      await db
+        .insertInto("article")
+        .values({
+          title: "foo",
+        })
+        .execute();
+
+      const found = await db
+        .selectFrom("article")
+        .selectAll()
+        .executeTakeFirstOrThrow();
+      assert.equal(found.created_at, found.updated_at);
+      assert.equal(typeof found.created_at === "string", true);
+    });
   });
 
-  it("find: bar", async () => {
-    const found_bar = await db
-      .selectFrom("user")
-      .where("name", "=", "bar")
-      .selectAll()
-      .executeTakeFirstOrThrow();
-
-    assert.equal(found_bar.name, "bar");
-    assert.equal(found_bar.json, null);
-  });
-
-  it("raw query", async () => {
-    type Row = { v: number };
-    const compiledQuery =
-      sql<Row>`select 1+2 as v, datetime('now') as now`.compile(db);
-    const output = await db.executeQuery(compiledQuery);
-    assert.equal(output.rows[0]?.v, 3);
+  describe("raw query", () => {
+    it("ok", async () => {
+      type Row = { v: number };
+      const compiledQuery =
+        sql<Row>`select 1+2 as v, datetime('now') as now`.compile(db);
+      const output = await db.executeQuery(compiledQuery);
+      assert.equal(output.rows[0]?.v, 3);
+    });
   });
 });
