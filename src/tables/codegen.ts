cbd8a671af52f16a92386266dfec73d2640bc30b
if1live <libsora25@gmail.com>
2023-12-12 00:38:45 +0900

    prisma-kysely 로 생성된 코드에 후처리해서 원하는 규격으로 만듬

--- src/tables/codegen.ts
+++ src/tables/codegen.ts
@@ -5,32 +5,32 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
 export type EventSourceMapping = {
-    id: Generated<number>;
-    uuid: string;
-    eventSourceArn: string;
-    functionArn: string;
-    payload: unknown;
-    createdAt: Generated<Timestamp>;
-    updatedAt: Generated<Timestamp>;
+  id: Generated<number>;
+  uuid: string;
+  eventSourceArn: string;
+  functionArn: string;
+  payload: unknown;
+  createdAt: Generated<Timestamp>;
+  updatedAt: Generated<Timestamp>;
 };
 export type FunctionDefinition = {
-    id: Generated<number>;
-    functionName: string;
-    functionArn: string;
-    payload: unknown;
-    createdAt: Generated<Timestamp>;
-    updatedAt: Generated<Timestamp>;
+  id: Generated<number>;
+  functionName: string;
+  functionArn: string;
+  payload: unknown;
+  createdAt: Generated<Timestamp>;
+  updatedAt: Generated<Timestamp>;
 };
 export type FunctionUrl = {
-    id: Generated<number>;
-    functionArn: string;
-    functionUrl: string;
-    payload: unknown;
-    createdAt: Generated<Timestamp>;
-    updatedAt: Generated<Timestamp>;
+  id: Generated<number>;
+  functionArn: string;
+  functionUrl: string;
+  payload: unknown;
+  createdAt: Generated<Timestamp>;
+  updatedAt: Generated<Timestamp>;
 };
 export type DB = {
-    eventSourceMapping: EventSourceMapping;
-    functionDefinition: FunctionDefinition;
-    functionUrl: FunctionUrl;
+  eventSourceMapping: EventSourceMapping;
+  functionDefinition: FunctionDefinition;
+  functionUrl: FunctionUrl;
 };
